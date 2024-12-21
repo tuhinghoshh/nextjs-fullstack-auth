9ecc518fe72ff22e6e5582f289235c735a5b6375
Tuhin Ghosh <tuhinkatwa8@gmail.com>
2024-12-21 19:06:56 +0530

    nextjsauth

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,7 +11,7 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.config({
-    extends: ["next/core-web-vitals", "next/typescript"],
+    extends: "next/core-web-vitals",
     rules: {
       "react/no-unescaped-entities": "off",
       "@next/next/no-page-custom-font": "off",
